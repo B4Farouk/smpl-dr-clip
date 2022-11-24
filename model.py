20d12dfb7fc178cf158e909235705c5398bf27ab
Jucifer06 <juliette.parchet@gmail.com>
2022-11-24 15:25:14 +0100

    add clip model

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
     This module regroups our functions and classes to build the
 """
 
-def build_model(smpl, renderer, clip):
+def build_model(smpl, renderer, clip_model):
     def smpl_fn(pose, shape):
         vertices, _ = smpl(pose, shape)
         return vertices
@@ -11,8 +11,8 @@ def build_model(smpl, renderer, clip):
         return renderer.render(mesh)
     
     def clip_fn(image, prompt):
-        # TODO
-        pass
+        similarity = clip_model.get_cosine_similarity(list(image), list(prompt))
+        return similarity[0][0]
     
     def model(pose, shape, prompt):
         return clip_fn(renderer_fn(smpl_fn(pose, shape)), prompt)
