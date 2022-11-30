4ba0a8f60b596c9b70ca515514ddb4d421d5c7ef
Farouk Boukil <farouk.boukil@epfl.ch>
2022-11-30 03:26:00 +0100

    updated clipmodel.py to be tested

--- clipmodel.py
+++ clipmodel.py
@@ -1,45 +1,58 @@
-import numpy as np
-from matplotlib import pyplot as plt
 import torch
-
 import clip
 
+import numpy as np
 
-class ClipModel:
+from scipy.spatial.distance import cosine
 
-    def __init__(self, model = "ViT-B/32"):
-        self.model, self.preprocess = clip.load(model)
-        self.model.cuda().eval()
+from matplotlib import pyplot as plt
 
-    def preprocess_images(self, images):
-        prep_images = (self.preprocess(img) for img in images)
-        return torch.tensor(np.stack(prep_images)).cuda()
+class CLIP:
+    __DEFAULT_MODEL_NAME = "ViT-B/32"
+    def __init__(self, model_name, device):
+        # save device
+        self.__device = device
+        # use default if name is None
+        model_name = model_name if model_name is not None else CLIPwrapper.__DEFAULT_MODEL_NAME
+        # load model
+        self.__model, self.__preprocess = clip.load(model_name)
+        self.__model.to(self.__device)
+        # switch to training mode
+        self.__model.train()
+        
+    def eval(self):
+        self.__model.eval()
+    
+    def train(self):
+        self.__model.train()
 
-    def tokenize_texts(self, texts):
-        return clip.tokenize(["This is " + desc for desc in texts]).cuda()
+    def preproc_image(self, image):
+        preproc_img = self.preprocess(image)
+        return torch.tensor(np.stack(preproc_img)).to(self.__device)
 
-    def get_features(self, image_tensor, tokenized_texts):
-        return (self.model.encode_image(image_tensor).float(), self.model.encode_text(tokenized_texts).float())
+    def tokenize_prompt(self, prompt):
+        return clip.tokenize(prompt).to(self.__device)
 
-    def get_cosine_similarity(self, images, texts):
-        # Preprocess the images and tokenize the texts
-        prep_images_tensor = self.preprocess_images(images)
-        tokenized_texts = self.tokenize_texts(texts)
+    def get_image_embedding(self, image):
+        return self.__model.encode_image(image).float()
 
-        # Extract features
+    def get_prompt_embedding(self, tokenized_prompt):
+        prompt_feat_vect = self.__model.encode_text(tokenized_prompt).float()
+        return prompt_feat_vect
+
+    def cos_dists(self, image, prompt):
+        # preprocess the images and tokenize the prompts
+        preproc_images    = self.preproc_image(image)
+        tokenized_prompts = self.tokenize_prompt(prompt)
+
+        # extract image and prompt embeddings (feature vectors)
         with torch.no_grad():
-            image_features, text_features = self.get_features(prep_images_tensor, tokenized_texts)
+            image_embedding = self.get_image_embedding(preproc_images)
+            prompt_embedding = self.get_prompt_embedding(tokenized_prompts)
+        
+        return cosine(image_embedding, prompt_embedding)
 
-        # Normalize the features
-        image_features /= image_features.norm(dim=-1, keepdim=True)
-        text_features /= text_features.norm(dim=-1, keepdim=True)
-
-        # Compute the cosine similarity
-        similarity = text_features.cpu().numpy() @ image_features.cpu().numpy().T
-
-        return similarity
-
-
+    @staticmethod
     def plot_cosine_similarity(similarity, images, texts, fig_size = (20, 14)):
         count = len(texts)
 
