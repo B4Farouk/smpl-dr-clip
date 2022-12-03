e7a355b975cb47d47349dd1b5ffdabd050a05d73
Farouk Boukil <farouk.boukil@epfl.ch>
2022-12-03 12:44:28 +0100

    fixed issuer: confusing similarity for distance in OptimEnv

--- optimization.py
+++ optimization.py
@@ -6,7 +6,9 @@ import torch.optim as optim
 
 
 class OptimEnv:
-    __DEFAULT_LOSS_FN = nn.CosineSimilarity(dim=1, eps=1e-12)
+    __DEFAULT_SIM_FN  = nn.CosineSimilarity(dim=1, eps=1e-12)
+    __DEFAULT_LOSS_FN = lambda img_emb, prompt_emb: 1 - OptimEnv.__DEFAULT_SIM_FN(img_emb, prompt_emb)
+    
     def __init__(self, model, params, lr=1, betas=(0.9, 0.999)):
         self.__model = model
         self.__params = params
@@ -19,8 +21,8 @@ class OptimEnv:
     def set_loss_fn(self, loss_fn):
         self.__loss_fn = loss_fn
     
-    def forward(self, pose, shape, prompt):
-        image_embedding, prompt_embedding = self.__model(pose, shape, prompt)
+    def forward(self, pose, shape):
+        image_embedding, prompt_embedding = self.__model(pose, shape)
         loss = self.__loss_fn(image_embedding, prompt_embedding)
         return loss
         
@@ -29,9 +31,9 @@ class OptimEnv:
         loss.backward()
         self.__optimizer.step()
     
-    def optimize(self, pose, shape, prompt, n_passes=1000, verbose=True):
+    def optimize(self, pose, shape, n_passes=1000, verbose=True):
         for n in range(n_passes):
-            loss = self.forward(pose, shape, prompt)
+            loss = self.forward(pose, shape)
             self.backward(loss)
             
             if verbose:
