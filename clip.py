63600754e89c2de77f76f6dab2ccc9cf2462d088
Farouk Boukil <farouk.boukil@epfl.ch>
2022-12-03 12:46:57 +0100

    updated image transform to avoid Compose

--- clip.py
+++ clip.py
@@ -5,9 +5,7 @@ from torchvision.transforms import Compose, Normalize
 
 class CLIPwrapper:
     __DEFAULT_MODEL_NAME = "ViT-B/32"
-    __IMAGE_TRANSFORM = Compose(
-        Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
-    )
+    __IMAGE_TRANSFORM = Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
         
     def __init__(self, model_name, device):
         # save device
