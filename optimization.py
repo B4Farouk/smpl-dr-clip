4a13d1b7ef12924308954f13f8edd228eaa9458c
Farouk Boukil <farouk.boukil@epfl.ch>
2022-12-03 15:02:02 +0100

    fixed typo

--- optimization.py
+++ optimization.py
@@ -33,7 +33,7 @@ class OptimEnv:
         
     def backward(self, loss):
         self.__optimizer.zero_grad()
-        loss.backward(retain_grad=True)
+        loss.backward(retain_graph=True)
         self.__optimizer.step()
     
     def optimize(self, pose, shape, n_passes=1000, verbose=True):
