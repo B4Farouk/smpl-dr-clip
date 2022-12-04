85946c9f1aea3aa12ddef73a1757b083e86425fd
Farouk Boukil <farouk.boukil@epfl.ch>
2022-12-04 14:33:01 +0100

    updated plots

--- plots.py
+++ plots.py
@@ -12,7 +12,7 @@ def plot_image_t(img_t):
 
 def plot_losses(losses):
     _, ax = plt.subplots(1, 1, figsize=(10,10))
-    sns.lineplot(data=losses, x="pass_num", y="loss", ax=ax)
+    sns.lineplot(data=losses, x="pass", y="loss", ax=ax)
     ax.set_title("Distance Between Image and Prompt Embeddings")
     ax.set_xlabel("pass number")
     ax.set_ylabel("distance")
