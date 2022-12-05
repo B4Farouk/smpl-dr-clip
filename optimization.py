5210d0c17e776434f7499e1b008f685ed938902f
Farouk Boukil <farouk.boukil@epfl.ch>
2022-12-05 15:13:39 +0100

    updated optimizer

--- optimization.py
+++ optimization.py
@@ -70,10 +70,10 @@ class OptimEnv:
                 intermediate_losses.loc[len(intermediate_losses)] = {"pass": n, "loss": loss.item()}
             # pose tracking
             if track_pose and n % pose_interleaving == 0:
-                intermediate_poses.loc[len(intermediate_poses)] = {"pass": n, "pose": pose.cpu().detach()}
+                intermediate_poses.loc[len(intermediate_poses)] = {"pass": n, "pose": pose.clone().detach()}
             # shape tracking
             if track_shape and n % shape_interleaving == 0:
-                intermediate_shapes.loc[len(intermediate_shapes)] = {"pass": n, "pose": shape.cpu().detach()}
+                intermediate_shapes.loc[len(intermediate_shapes)] = {"pass": n, "pose": shape.clone().detach()}
         
         # if tracker_settings is None: result["tracked"] is None
         #   if not(track_loss): result["tracked"]["losses"] is None,
@@ -82,7 +82,8 @@ class OptimEnv:
             "params": {"pose": pose, "shape": shape},
             "tracked": {
                 "losses": intermediate_losses if track_loss else None, 
-                "poses": intermediate_poses if track_pose else None
+                "poses": intermediate_poses if track_pose else None,
+                "shapes": intermediate_shapes if track_shape else None
                 } if tracker_settings is not None else None
         }
         
