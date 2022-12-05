bc0eab454de1e7148a442ed4fd005cabc0855204
Farouk Boukil <farouk.boukil@epfl.ch>
2022-12-05 14:56:42 +0100

    renaming of config to context

--- model.py
+++ model.py
@@ -27,7 +27,7 @@ def compose(smpl, renderer, clip, **params):
         
     return model
 
-class SimpledCLIPConfig:
+class SimpledCLIPContext:
     def __init__(self, smpl, renderer, clip):
         self.__smpl = smpl
         self.__renderer = renderer
