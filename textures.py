cc96745f45cc232ef718bf11394b693e5f14f4d5
Aco <56964728+Aco-Hub@users.noreply.github.com>
2022-12-06 18:11:34 +0100

    Update textures.py

--- textures.py
+++ textures.py
@@ -41,7 +41,7 @@ class TexturesFactory:
     
     def from_image(self,colored_reference_SMPL,verts,faces):
         
-        random_SMPL = trimesh.Trimesh(verts[0], faces, process=False)
+        random_SMPL = trimesh.Trimesh(verts[0].cpu(), faces.cpu(), process=False)
         random_SMPL.visual.vertex_colors = colored_reference_SMPL.visual.vertex_colors
         texture = torch.from_numpy(
             colored_reference_SMPL.visual.vertex_colors[:,:3] # Remove transparency
