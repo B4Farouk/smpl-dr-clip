e71f058924c9cbb3aaf4e634cb93dfc8a462f6eb
Farouk Boukil <farouk.boukil@epfl.ch>
2022-12-10 14:29:58 +0100

    updated smpl model for multi-rendering

--- smpl.py
+++ smpl.py
@@ -18,16 +18,17 @@ def mesh_from(vertices, faces, texture):
         textures=texture)
 
 class SMPLwrapper:
-    __DEFAULT_MODEL = SMPL_Layer(gender='female',
-        model_root='project')
+    __DEFAULT_MODEL = SMPL_Layer(gender='female', model_root='project')
     
-    def __init__(self, model, txmapping, device):
+    def __init__(self, model, txmapping, batchsize, device):
         device = device if device is not None else torch.device("cpu")        
         
         self.__txmapping = txmapping
         
         self.__model = model if model is not None else SMPLwrapper.__DEFAULT_MODEL
         self.__model.to(device)
+        
+        self.__batchsize = 1 if batchsize is None or batchsize <= 0 else batchsize
                  
     # theta is the pose parameter of shape (1,72) 
     # beta is the shape parameter of shape (1,10)
@@ -37,7 +38,7 @@ class SMPLwrapper:
         faces = self.__model.th_faces[None, :]
         return vertices, faces
     
-    def meshes(self, theta, beta, batchsize=1):        
+    def meshes(self, theta, beta):        
         verts, faces = self.verts_and_faces(theta, beta)
         texture = self.__txmapping(verts, faces)
         mesh = mesh_from(
@@ -45,5 +46,5 @@ class SMPLwrapper:
             faces=faces, 
             texture=texture)
         
-        meshes = mesh.extend(batchsize) if batchsize > 1 else mesh
+        meshes = mesh.extend(self.__batchsize) if self.__batchsize > 1 else mesh
         return meshes
