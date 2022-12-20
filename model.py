ab7429efd5060b0614039d9c51e615211eee9ab7
Farouk Boukil <farouk.boukil@epfl.ch>
2022-12-20 19:55:40 +0100

    commented model.py

--- model.py
+++ model.py
@@ -3,15 +3,20 @@
 """
 
 def compose(smpl, renderer, clip, **params):
-    """_summary_
+    """
+    Composes an instance of SMPL, the differentiable renderer and CLIP
+    with a hyperparameter prompt, into a single instance of SimpledCLIP 
+    (in the form of a Callable).
 
     Args:
-        smpl (_type_): _description_
-        renderer (_type_): _description_
-        clip (_type_): _description_
+        smpl: an instance of an SMPL model.
+        renderer: an instance of a differentiable renderer.
+        clip: an instance of a CLIP model.
+        **params:
+            prompt: the prompt containing a pose description to be recovered.
 
     Returns:
-        _type_: _description_
+        A SimpledCLIP model, in the form of a Callable
     """
     # the prompt as a hyperparameter
     prompt = params["prompt"]
@@ -38,10 +43,22 @@ def compose(smpl, renderer, clip, **params):
     return model
 
 class SimpledCLIPContext:
+    """
+    Represents a context for creating SimpledCLIP instances using the same models.
+    """
     def __init__(self, smpl, renderer, clip):
         self.__smpl = smpl
         self.__renderer = renderer
         self.__clip = clip
         
     def get_model(self,  prompt):
+        """
+        Creates a SimpledCLIP model for a given prompt using the underlying models.
+        
+        Args:
+            prompt: the prompt containing a pose description to be recovered.
+            
+        Returns:
+            A SimpledCLIP model, in the form of a Callable
+        """
         return compose(self.__smpl, self.__renderer, self.__clip, prompt=prompt)
